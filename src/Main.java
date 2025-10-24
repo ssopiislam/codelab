4ba09be913a5d3f4a992e59b030480efb5d61f28
sopi <sopisofi22@gmail.com>
2025-10-24 16:48:15 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class InvalidNumberException extends Exception {
+    public InvalidNumberException(String message) {
+        super(message);
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Masukkan angka: ");
+            int angka = sc.nextInt();
+
+            if (angka <= 0) {
+                throw new InvalidNumberException("Angka tidak valid! Harus lebih dari 0.");
+            }
+
+            System.out.println("Valid! Angka Positif: " + angka);
+
+        } catch (InvalidNumberException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Input harus berupa angka!");
+        } finally {
+            sc.close();
+        }
+    }
+}
